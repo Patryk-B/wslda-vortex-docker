5243aef72f41bd2852d2f4358fac32e7a524b4c7
Patryk-B <patryk.bechcicki@gmail.com>
2024-10-26 22:17:12 +0200

    feat: plots current density

--- projects/wslda-vortex-analysis/src/start.py
+++ projects/wslda-vortex-analysis/src/start.py
@@ -12,7 +12,7 @@ import matplotlib.figure
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
-import numpy.typing
+import numpy.typing as np_typing
 import pprint
 import sys
 
@@ -40,32 +40,37 @@ class ParsedWData(object):
     ):
         # data:
         # - grid:
+
         self.Nx: int = wdata.Nxyz[Axis.X]
         self.Ny: int = wdata.Nxyz[Axis.Y]
         self.dx: float = wdata.dxyz[Axis.X]
         self.dy: float = wdata.dxyz[Axis.Y]
-        self.x: numpy.ndarray = wdata.xyz[Axis.X]
-        self.y: numpy.ndarray = wdata.xyz[Axis.Y]
+        self.x: np.ndarray = wdata.xyz[Axis.X]
+        self.y: np.ndarray = wdata.xyz[Axis.Y]
         self.x_flat: List[float] = self.x.flatten()
         self.y_flat: List[float] = self.y.flatten()
 
         # data:
         # - current density
-        self.j_a_x: numpy.memmap = wdata.j_a[iteration][Component.X]
-        self.j_a_y: numpy.memmap = wdata.j_a[iteration][Component.Y]
-        self.j_a: List[List[float]] = [[j_a_x, j_a_y] for j_a_x, j_a_y in zip(self.j_a_x, self.j_a_y)]
-        self.j_b_x: numpy.memmap = wdata.j_b[iteration][Component.X]
-        self.j_b_y: numpy.memmap = wdata.j_b[iteration][Component.Y]
-        self.j_b: List[List[float]] = [[j_b_x, j_b_y] for j_b_x, j_b_y in zip(self.j_b_x, self.j_b_y)]
+
+        self.j_a_x: np.memmap = wdata.j_a[iteration][Component.X]
+        self.j_a_y: np.memmap = wdata.j_a[iteration][Component.Y]
+        self.j_a: np.ndarray = np.column_stack((self.j_a_x, self.j_a_y))
+        self.j_a_magnitude: np.ndarray = np.sqrt(self.j_a_x ** 2 + self.j_a_y ** 2)
+
+        self.j_b_x: np.memmap = wdata.j_b[iteration][Component.X]
+        self.j_b_y: np.memmap = wdata.j_b[iteration][Component.Y]
+        self.j_b: np.ndarray = np.column_stack((self.j_b_x, self.j_b_y))
+        self.j_b_magnitude: np.ndarray = np.sqrt(self.j_b_x ** 2 + self.j_b_y ** 2)
 
 
 class Plot():
     def __init__(
         self,
-        rows_h_ratios,
-        cols_w_ratios,
-        subplot_h,
-        subplot_w,
+        rows_h_ratios: List[int],
+        cols_w_ratios: List[int],
+        subplot_h: int,
+        subplot_w: int,
     ):
         # plot's width and height:
         self.subplot_h: float = subplot_h
@@ -93,17 +98,6 @@ class Plot():
                 self.ax[i][j] = self.fig.add_subplot(self.gs[i, j])
 
 
-class CustomScientificFormatter(ticker.ScalarFormatter):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-    def __call__(self, x, pos=None):
-        if x == 0:
-            return "0"  # Handle zero separately
-        else:
-            return f"{x:.1e}"  # Format to scientific notation, e.g., 1.0e-3
-
-
 def gen_scientific_formatter() -> ticker.Formatter:
     formatter = ticker.ScalarFormatter(useMathText=True)
     formatter.set_scientific(True)
@@ -118,46 +112,54 @@ def gen_scientific_formatter() -> ticker.Formatter:
 def plot_streamplot(
     fig: matplotlib.figure.Figure,
     ax: matplotlib.axes.Axes,
-    x: numpy.typing.ArrayLike,
-    y: numpy.typing.ArrayLike,
-    data_x: numpy.typing.ArrayLike,
-    data_y: numpy.typing.ArrayLike,
-    title,
-    label_x,
-    label_y,
+    x: np_typing.ArrayLike,
+    y: np_typing.ArrayLike,
+    data_x: np_typing.ArrayLike,
+    data_y: np_typing.ArrayLike,
+    title: str,
+    label_x: str,
+    label_y: str,
 ) -> None:
     # Plot data:
     ax.streamplot(x, y, data_x, data_y, color='blue', linewidth=2, density=1.5)
-    ax.set_aspect('equal', adjustable='box')
+
+    # Labels
     ax.set_title(title)
     ax.set_xlabel(label_x)
     ax.set_ylabel(label_y)
 
-    # Set axis limits
+    # Axis limits
     ax.set_xlim(np.min(x), np.max(x))
     ax.set_ylim(np.min(y), np.max(y))
+
+    # # Aspect ratio:
+    # ax.set_aspect('equal', adjustable='box')
 
 
 def plot_pseudocolor(
     fig: matplotlib.figure.Figure,
     ax: matplotlib.axes.Axes,
-    x: numpy.typing.ArrayLike,
-    y: numpy.typing.ArrayLike,
-    data: numpy.typing.ArrayLike,
-    title,
-    label_x,
-    label_y,
+    x: np_typing.ArrayLike,
+    y: np_typing.ArrayLike,
+    data: np_typing.ArrayLike,
+    title: str,
+    label_x: str,
+    label_y: str,
 ) -> None:
     # Plot data:
     pcolor = ax.pcolormesh(x, y, data, cmap='plasma', shading='auto')
-    ax.set_aspect('equal', adjustable='box')
+
+    # Labels
     ax.set_title(title)
     ax.set_xlabel(label_x)
     ax.set_ylabel(label_y)
 
-    # Set axis limits
+    # Axis limits
     ax.set_xlim(np.min(x), np.max(x))
     ax.set_ylim(np.min(y), np.max(y))
+
+    # # Aspect ratio:
+    # ax.set_aspect('equal', adjustable='box')
 
     # Add color bar
     color_bar = fig.colorbar(
@@ -182,20 +184,20 @@ def plot_crossections_of_2D_data(
     fig: matplotlib.figure.Figure,
     ax: matplotlib.axes.Axes,
     indices: List[int],
-    x: numpy.typing.ArrayLike,
-    data: numpy.typing.ArrayLike,
-    gen_csec_func: Callable[[numpy.typing.ArrayLike, int], numpy.typing.ArrayLike],
-    title,
-    label_x,
-    label_y,
-    gen_label_data_func: Callable[[numpy.typing.ArrayLike, int], str],
+    x: np_typing.ArrayLike,
+    data: np_typing.ArrayLike,
+    gen_data_csec_func: Callable[[np_typing.ArrayLike, int], np_typing.ArrayLike],
+    title: str,
+    label_x: str,
+    label_y: str,
+    gen_label_data_func: Callable[[np_typing.ArrayLike, int], str],
 ) -> None:
     # The default color cycle
     colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 
     # Plot data:
     for i, color in zip(indices, colors):
-        data_csec = gen_csec_func(data, i)
+        data_csec = gen_data_csec_func(data, i)
 
         int_x = np.linspace(x[0], x[-1], 200)
         int_data = interp1d(x, data_csec, kind='quadratic')
@@ -229,7 +231,7 @@ def plot_crossections_of_2D_data(
     ax.set_xlim(1.05 * np.min(x), 1.05 * np.max(x))
     ax.set_ylim(1.05 * np.min(data), 1.05 * np.max(data))
 
-    # Format axis labels to scientific notation
+    # Scientific notation
     formatter = gen_scientific_formatter()
     ax.xaxis.set_major_formatter(formatter)
     ax.yaxis.set_major_formatter(formatter)
@@ -243,15 +245,16 @@ def plot_crossections_of_2D_data(
 
 def plot_current_density(
     wdata: WData,
-    iteration: int
-):
+    iteration: int,
+    out_dir: str,
+) -> None:
     # parse WData:
     d = ParsedWData(wdata, iteration)
 
     # gen plot:
     p = Plot(
-        cols_w_ratios = [1, 1, 1, 1, 1, 1, 1],
-        rows_h_ratios = [1, 1],
+        cols_w_ratios = [1, 1, 1],
+        rows_h_ratios = [1, 1, 1, 1, 1, 1],
         subplot_w = 10,
         subplot_h = 10,
     )
@@ -269,29 +272,31 @@ def plot_current_density(
     y_crossections_02 = [ny_half - 10, ny_half - 5, ny_half - 2, ny_half, ny_half + 2, ny_half + 5, ny_half + 10]
 
     plot_streamplot(p.fig, p.ax[0][0], d.x_flat, d.y_flat, d.j_a_x.T, d.j_a_y.T, 'j_a', 'x', 'y')
-    plot_streamplot(p.fig, p.ax[1][0], d.x_flat, d.y_flat, d.j_b_x.T, d.j_b_y.T, 'j_b', 'x', 'y')
+    plot_pseudocolor(p.fig, p.ax[0][1], d.x_flat, d.y_flat, d.j_a_magnitude.T, '|| j_a ||', 'x', 'y')
 
-    plot_pseudocolor(p.fig, p.ax[0][1], d.x_flat, d.y_flat, d.j_a_x.T, 'j_a_x', 'x', 'y')
-    plot_pseudocolor(p.fig, p.ax[0][4], d.x_flat, d.y_flat, d.j_a_y.T, 'j_a_y', 'x', 'y')
-    plot_pseudocolor(p.fig, p.ax[1][1], d.x_flat, d.y_flat, d.j_b_x.T, 'j_b_x', 'x', 'y')
-    plot_pseudocolor(p.fig, p.ax[1][4], d.x_flat, d.y_flat, d.j_b_y.T, 'j_b_y', 'x', 'y')
+    plot_pseudocolor(p.fig, p.ax[1][0], d.x_flat, d.y_flat, d.j_a_x.T, 'j_a_x', 'x', 'y')
+    plot_crossections_of_2D_data(p.fig, p.ax[1][1], x_crossections_01, d.y_flat, d.j_a_x, lambda data, i: data[i, :], 'j_a_x', 'y', 'j_a_x', lambda x, i: f"j_a_x(x = {x[i]}, y)")
+    plot_crossections_of_2D_data(p.fig, p.ax[1][2], y_crossections_02, d.x_flat, d.j_a_x, lambda data, i: data[:, i], 'j_a_x', 'x', 'j_a_x', lambda y, i: f"j_a_x(x, y = {y[i]})")
 
-    plot_crossections_of_2D_data(p.fig, p.ax[0][2], x_crossections_01, d.y_flat, d.j_a_x, lambda data, i: data[i, :], 'j_a_x', 'y', 'j_a_x', lambda x, i: f"j_a_x(x = {x[i]}, y)")
-    plot_crossections_of_2D_data(p.fig, p.ax[0][3], y_crossections_02, d.x_flat, d.j_a_x, lambda data, i: data[:, i], 'j_a_x', 'x', 'j_a_x', lambda y, i: f"j_a_x(x, y = {y[i]})")
+    plot_pseudocolor(p.fig, p.ax[2][0], d.x_flat, d.y_flat, d.j_a_y.T, 'j_a_y', 'x', 'y')
+    plot_crossections_of_2D_data(p.fig, p.ax[2][1], x_crossections_02, d.y_flat, d.j_a_y, lambda data, i: data[i, :], 'j_a_y', 'y', 'j_a_y', lambda x, i: f"j_a_y(x = {x[i]}, y)")
+    plot_crossections_of_2D_data(p.fig, p.ax[2][2], y_crossections_01, d.x_flat, d.j_a_y, lambda data, i: data[:, i], 'j_a_y', 'x', 'j_a_y', lambda y, i: f"j_a_y(x, y = {y[i]})")
 
-    plot_crossections_of_2D_data(p.fig, p.ax[0][5], x_crossections_02, d.y_flat, d.j_a_y, lambda data, i: data[i, :], 'j_a_y', 'y', 'j_a_y', lambda x, i: f"j_a_y(x = {x[i]}, y)")
-    plot_crossections_of_2D_data(p.fig, p.ax[0][6], y_crossections_01, d.x_flat, d.j_a_y, lambda data, i: data[:, i], 'j_a_y', 'x', 'j_a_y', lambda y, i: f"j_a_y(x, y = {y[i]})")
+    plot_streamplot(p.fig, p.ax[3][0], d.x_flat, d.y_flat, d.j_b_x.T, d.j_b_y.T, 'j_b', 'x', 'y')
+    plot_pseudocolor(p.fig, p.ax[3][1], d.x_flat, d.y_flat, d.j_b_magnitude.T, '|| j_b ||', 'x', 'y')
 
-    plot_crossections_of_2D_data(p.fig, p.ax[1][2], x_crossections_01, d.y_flat, d.j_b_x, lambda data, i: data[i, :], 'j_b_x', 'y', 'j_b_x', lambda x, i: f"j_b_x(x = {x[i]}, y)")
-    plot_crossections_of_2D_data(p.fig, p.ax[1][3], y_crossections_02, d.x_flat, d.j_b_x, lambda data, i: data[:, i], 'j_b_x', 'x', 'j_b_x', lambda y, i: f"j_b_x(x, y = {y[i]})")
+    plot_pseudocolor(p.fig, p.ax[4][0], d.x_flat, d.y_flat, d.j_b_x.T, 'j_b_x', 'x', 'y')
+    plot_crossections_of_2D_data(p.fig, p.ax[4][1], x_crossections_01, d.y_flat, d.j_b_x, lambda data, i: data[i, :], 'j_b_x', 'y', 'j_b_x', lambda x, i: f"j_b_x(x = {x[i]}, y)")
+    plot_crossections_of_2D_data(p.fig, p.ax[4][2], y_crossections_02, d.x_flat, d.j_b_x, lambda data, i: data[:, i], 'j_b_x', 'x', 'j_b_x', lambda y, i: f"j_b_x(x, y = {y[i]})")
 
-    plot_crossections_of_2D_data(p.fig, p.ax[1][5], x_crossections_02, d.y_flat, d.j_b_y, lambda data, i: data[i, :], 'j_b_y', 'y', 'j_b_y', lambda x, i: f"j_b_y(x = {x[i]}, y)")
-    plot_crossections_of_2D_data(p.fig, p.ax[1][6], y_crossections_01, d.x_flat, d.j_b_y, lambda data, i: data[:, i], 'j_b_y', 'x', 'j_b_y', lambda y, i: f"j_b_y(x, y = {y[i]})")
+    plot_pseudocolor(p.fig, p.ax[5][0], d.x_flat, d.y_flat, d.j_b_y.T, 'j_b_y', 'x', 'y')
+    plot_crossections_of_2D_data(p.fig, p.ax[5][1], x_crossections_02, d.y_flat, d.j_b_y, lambda data, i: data[i, :], 'j_b_y', 'y', 'j_b_y', lambda x, i: f"j_b_y(x = {x[i]}, y)")
+    plot_crossections_of_2D_data(p.fig, p.ax[5][2], y_crossections_01, d.x_flat, d.j_b_y, lambda data, i: data[:, i], 'j_b_y', 'x', 'j_b_y', lambda y, i: f"j_b_y(x, y = {y[i]})")
 
     # save plot:
     p.fig.tight_layout()
     p.fig.savefig(
-        fname = f"plot_current_density.png",
+        fname = out_dir + "/plot_current_density.png",
         dpi = 300
     )
 
@@ -304,10 +309,10 @@ def main() -> int:
     entry point
     """
 
-    inDir = "/workspace/results/data"
-    outDir = "/workspace/results/analysis"
-    inFile = inDir + "/st-vortex-recreation-01/sv-ddcc05p00-T0p05.wtxt"
-    wdata = WData.load(inFile)
+    in_dir = "/workspace/results/data"
+    out_dir = "/workspace/results/analysis"
+    in_file = in_dir + "/st-vortex-recreation-01/sv-ddcc05p00-T0p05.wtxt"
+    wdata = WData.load(in_file)
 
     # pprint.pp(vars(data))
     # pprint.pp(data.aliases)
@@ -319,7 +324,8 @@ def main() -> int:
     # plot:
     plot_current_density(
         wdata = wdata,
-        iteration = iteration
+        iteration = iteration,
+        out_dir = out_dir
     )
 
     return 0
